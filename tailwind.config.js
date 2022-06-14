a41a6244cabeb789f6bd06deb773d83c3b9c2ae2
Misael Burboa <mburboa@consumertrack.com>
2022-06-14 10:24:29 -0700

    a bit more of configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 40rem) { ... }
+
+      md: '768px',
+      // => @media (min-width: 48rem) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 64rem) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 80rem) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 96rem) { ... }
+    },
   },
   plugins: [],
 }
