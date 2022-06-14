add94ac72a598976ddaba18cadbbabf10662bc2d
Misael Burboa <mburboa@consumertrack.com>
2022-06-14 11:47:33 -0700

    remove the scss unknown-at-rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -38,12 +38,6 @@ module.exports = {
       },
     ],
     'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['tailwind'],
-      },
-    ],
 
     //
     // stylelint
